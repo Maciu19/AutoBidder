f276a9e49414021ce6769342f2e3a169ec942a0e
Hubati Cristian <cristihubati03@gmail.com>
2025-04-05 21:47:45 +0300

    Inital Commit and structure

---
+++ autobidder-api/src/main/java/com/maciu19/autobidder_api/AutobidderApiApplication.java
@@ -0,0 +1,13 @@
+package com.maciu19.autobidder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobidderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobidderApiApplication.class, args);
+	}
+
+}
---
+++ autobidder-api/src/test/java/com/maciu19/autobidder_api/AutobidderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciu19.autobidder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobidderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
